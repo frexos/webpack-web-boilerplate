616c8ca97011dd8c949e2fd839eacb6a4790df1a
e.georgantzos <e.georgantzos@exus.co.uk>
2017-06-08 10:31:40 +0300

    add and test jquery

---
+++ app/scripts/home.js
@@ -0,0 +1,9 @@
+import $ from 'jQuery';
+
+(function () {
+
+    $(document).ready(function() {
+        console.log('It works!');
+    });
+
+})();
--- webpack.parts.js
+++ webpack.parts.js
@@ -1,3 +1,5 @@
+var webpack = require('webpack');
+
 exports.devServer = ({ host, port } = {}) => ({
     devServer: {
         historyApiFallback: true,
